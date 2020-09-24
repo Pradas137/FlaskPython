1f3603d017553689c03566b75b0d4619b5a70cf7
Pradas137 <55705052+Pradas137@users.noreply.github.com>
2020-09-24 23:43:55 +0200

    nuevo12

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from flask import Flask
 app = Flask(__name__)
 
+from collections import OrderedDict
 from flask import render_template
 from flask import request
 from flask import current_app
@@ -21,12 +22,12 @@ def file():
 def partido(file):
     equipos = []
     for Local in file:
-        # print(Local)
+       # print(Local)
         for Visitantes in file:
             # print(Visitantes)
             if Local != Visitantes:
                 equipos.append(Visitantes)
-                #print(equipos)
+                #print(equipos) 
     Ligas ={}
     for Local in range(0, len(file)):
         Lista = []
@@ -58,10 +59,25 @@ def añadir(Partidos):
             Visitantes.update(temp)
     return(Partidos)
 
+def Mostrar(Partidos):
+    equips = open("equips.cfg", "r")
+    datos = equips.read().splitlines()
+    for j in range(0, len(datos)):
+        datos[j] = {datos[j]: 0}
+    equips.close()
+    for Local in Partidos:
+        #print(local)
+        for pe in Partidos[Local]:
+            # print(pe)
+            for i in pe:
+                lo = (Local, i, (pe.get(i)))
+            print(lo)
+
 
 file=file()
 Partidos = partido(file)
 add= añadir(Partidos)
+mostrar= Mostrar(add)
 
 @app.route('/')
 def Menu():
@@ -69,7 +85,8 @@ def Menu():
 
 @app.route('/Equipos')
 def Equipos_get():
-        return render_template('Equipos.html',file=file)
+    data = [['Valencia',[['Betis', (6, 0)],['Madrid', (2, 7)],['Sevilla',(3, 7)]]],['Betis',[['Valencia', (3, 7)],['Madrid',  (7, 3)],['Sevilla',(8, 6)]]]]
+    return render_template('Equipos.html',Partidos=Partidos,file=file,mostrar=mostrar,data=data)
 
 @app.route('/Partidos')
 def Partidos_get():
