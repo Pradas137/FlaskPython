2a94108937dea80507673fca294395f9faa8f1e5
Pradas137 <55705052+Pradas137@users.noreply.github.com>
2020-10-02 11:07:38 +0200

    02102020

--- web.py
+++ web.py
@@ -20,11 +20,15 @@ def CrearLiga():
         Liga[Local] = {}
         for Visitante in equips:
             if Visitante == Local:
-                Liga[Local][Visitante] = "X"
+                Liga[Local][Visitante] = "x"
             else:
                 Liga[Local][Visitante] = ""
     return Liga
 
+def update_league(Local, Visitante, GolesL, GolesV):
+    """Update goals on the league matrix."""
+    Ligas[Local][Visitante] = GolesL
+    Ligas[Visitante][Local] = GolesV
 
 
 equips = Equipos()
@@ -40,66 +44,7 @@ def Lista_equipos():
     return render_template("Equipos.html", equips=equips)
 
 @ app.route('/Partidos')
-def league_grid():
+def Partidos():
     return render_template("Partidos.html", Ligas=Ligas, equips=equips)
 
-
-def update_league(local_team, visitant_team, local_goals, visitant_goals):
-    """Update goals on the league matrix."""
-    league[local_team][visitant_team] = local_goals
-    league[visitant_team][local_team] = visitant_goals
-
-
-def update_ranking():
-    """TODO: Work on a better function."""
-    global ranking
-    ranking = create_ranking()
-    done_teams = []
-    for local_team in league:
-        for visitant_team in league[local_team]:
-            if league[local_team][visitant_team] != "":
-                if local_team and visitant_team not in done_teams and local_team != visitant_team:
-                    points = check_ranking_points(
-                        league[local_team][visitant_team], league[visitant_team][local_team])
-                    set_ranking_points(points, local_team, visitant_team)
-        done_teams.append(local_team)
-
-
-def check_ranking_points(local, visitant):
-    """Check how many points did the local team win."""
-    if local != "":
-        if local > visitant:
-            return 3
-        elif local == visitant:
-            return 1
-        else:
-            return 0
-    else:
-        return 0
-
-
-def set_ranking_points(points, local_team, visitant_team):
-    """Sum the points depending on the condition."""
-    if points == 3:
-        ranking[local_team] += points
-    elif points == 1:
-        ranking[local_team] += points
-        ranking[visitant_team] += points
-    else:
-        ranking[local_team] += points
-        ranking[visitant_team] += 3
-
         
-@app.route('/Partidos', methods=["POST"])
-def goals_input_post():
-    Local = request.form["EquipoLocal"]
-    Visitante = request.form["EquipoVisitante"]
-    GolesL = request.form["PuntuacionLocal"]
-    GolesV = request.form["Puntuacionvisitante"]
-    if Local == visitante:
-        return goals_input("sameTeams")
-    else:
-        update_league(Local, Visitante, Golesl, GolesV)
-        return redirect(url_for("league_grid"))
-if __name__ == '__main__':
-    app.run()
